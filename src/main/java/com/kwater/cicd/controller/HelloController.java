dde7a779d28767c2c17c0c72264568d8d12a6f99
qkd1101 <qkd1101@naver.com>
2022-06-01 23:02:33 +0900

    update HelloController.java

--- src/main/java/com/kwater/cicd/controller/HelloController.java
+++ src/main/java/com/kwater/cicd/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello #1";
+        return "Hello #2";
     }
 }
