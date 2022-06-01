3b29cbde4d610cb609ea4bb30a49b6a57d39e1d5
qkd1101 <qkd1101@naver.com>
2022-06-01 15:46:00 +0900

    Initial commit

---
+++ src/main/java/com/kwater/cicd/KwaterApplication.java
@@ -0,0 +1,13 @@
+package com.kwater.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwater/cicd/KwaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwater.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
